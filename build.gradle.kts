1e6027152ff1074f774451b961849986901b5513
Marc Knaup <marc@knaup.io>
2020-08-12 13:33:01 +0100

    fluid-gradle 1.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import io.fluidsonic.gradle.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "1.1.0"
+	id("io.fluidsonic.gradle") version "1.1.4"
 }
 
 fluidLibrary(name = "locale", version = "0.9.0")
