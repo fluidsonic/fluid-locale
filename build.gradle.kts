00eb3c6db43205f54b9df5f48599dca088bfd895
Marc Knaup <marc@knaup.io>
2021-02-04 20:12:29 +0100

    Updated all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 import io.fluidsonic.gradle.*
+import org.jetbrains.kotlin.gradle.plugin.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "1.1.14"
+	id("io.fluidsonic.gradle") version "1.1.18"
 }
 
 fluidLibrary(name = "locale", version = "0.9.4")
@@ -9,7 +10,7 @@ fluidLibrary(name = "locale", version = "0.9.4")
 fluidLibraryModule(description = "Kotlin multiplatform locale library") {
 	targets {
 		darwin()
-		js()
+		js(KotlinJsCompilerType.BOTH)
 		jvm()
 	}
 }
