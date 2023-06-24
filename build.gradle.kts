87ef5f0c54cf9fa9ec57d3ffe0376af49305103c
Marc Knaup <marc@knaup.io>
2023-06-24 08:14:08 +0700

    🎉 0.13.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.3.1"
 }
 
-fluidLibrary(name = "locale", version = "0.12.0")
+fluidLibrary(name = "locale", version = "0.13.0")
 
 fluidLibraryModule(description = "Kotlin multiplatform locale library") {
 	targets {
